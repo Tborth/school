17105db9adb9d628407ffc5a4d42563b4370df8c
tborth <tarunchaudhary243@gmail.com>
2020-04-14 09:06:35 -0700

    odoo

--- models/student_models.py
+++ models/student_models.py
@@ -1,13 +1,16 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 import logging
 
 
 class Hospital_Details(models.Model):
     _name = "hospital.details"
 
-    hos_name = fields.Char(string='Name', required=True, copy=False)
-    number_bed = fields.Integer(string="number of bed")
+    hos_name = fields.Char(string='hos_Name', required=True, copy=False)
+    number_bed = fields.Integer(string="number_of_bed")
     email = fields.Char(string="Email")
+    
+
 
 class Corona_Details(models.Model):
 
@@ -17,6 +20,8 @@ class Corona_Details(models.Model):
     
     die_count =fields.Integer(string="die people")
 
+
+
 class Patient(models.Model):
 
     _name ="patient.details"
@@ -25,8 +30,26 @@ class Patient(models.Model):
 
     age =fields.Integer(string="Age")
 
-    mobile=fields.Integer(string="mobile number")
+    mobile=fields.Char(string="mobile number")
 
     disease =fields.Selection([('runing noise','runing noise'),('fever','fever'),('corona','corona'),])
     
     email=fields.Char(string='Email')
+
+    fee =fields.Float(compute='check_age')
+
+    @api.depends('age')
+    def check_age(self):
+        if self.age<18:
+            self.fee=500.00
+        else:
+            self.fee=1000.00
+    
+    @api.constrains('mobile')
+    def check_mobile_number(self):
+        if not self.mobile.isdigit():
+            raise ValidationError("Please enter valid mobile number")
+        if len(self.contact)!=10:
+            raise ValidationError("legth of number should be 10")
+
+    
